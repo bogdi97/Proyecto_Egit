8b685e0da87c17ac56f2823f40662a730e5432e7
bogdi97 <59975@iessierradeguara.com>
2018-02-21 13:19:01 +0100

    Crea una clase Hola mundo
    Añade un paquete holamundo y dentro una clase llamada holamundo con un
    main que imprime el mensaje Hola Mundo.

---
+++ ProyectoGIT/src/holamundo/holamundo.java
@@ -0,0 +1,10 @@
+package holamundo;
+import javax.swing.*;
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Hola Mundo");
+	}
+
+}
